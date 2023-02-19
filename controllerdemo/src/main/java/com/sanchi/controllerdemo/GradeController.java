a3e6242a76b7f0866f4d0d06ed44a8bb9b63fba4
sanchi285 <bismoy.sanchi@gmail.com>
2023-02-20 00:18:05 +0600

    merged model data with the view

--- controllerdemo/src/main/java/com/sanchi/controllerdemo/GradeController.java
+++ controllerdemo/src/main/java/com/sanchi/controllerdemo/GradeController.java
@@ -1,11 +1,16 @@
 package com.sanchi.controllerdemo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class GradeController {
     @GetMapping("/grades")
-    public String sayHello(){
+    public String sayHello(Model model){
+        Grade grade = new Grade("Harry","potion","A+");
+        model.addAttribute("grade",grade);
         return "grades";
     }
 }
