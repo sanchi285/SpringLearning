bb18c0d2f4d86b9a15467aec173aebf48ed85d7c
sanchi285 <bismoy.sanchi@gmail.com>
2023-02-19 23:09:22 +0600

    Controller and view done

---
+++ controllerdemo/src/main/java/com/sanchi/controllerdemo/GradeController.java
@@ -0,0 +1,11 @@
+package com.sanchi.controllerdemo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GradeController {
+    @GetMapping("/grades")
+    public String sayHello(){
+        return "grades";
+    }
+}
