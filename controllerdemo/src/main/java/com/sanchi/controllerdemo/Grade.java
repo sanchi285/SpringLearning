d4b5a2b48645b7c4a71090fcb8311f6738dbf678
sanchi285 <bismoy.sanchi@gmail.com>
2023-02-19 23:18:33 +0600

    POJO Class created

---
+++ controllerdemo/src/main/java/com/sanchi/controllerdemo/Grade.java
@@ -0,0 +1,42 @@
+package com.sanchi.controllerdemo;
+
+public class Grade {
+
+    private String name;
+    private String sub;
+    private String score;
+
+    public Grade(String name, String sub, String score) {
+        this.name = name;
+        this.sub = sub;
+        this.score = score;
+    }
+
+    public Grade() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSub() {
+        return this.sub;
+    }
+
+    public void setSub(String sub) {
+        this.sub = sub;
+    }
+
+    public String getScore() {
+        return this.score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+}
