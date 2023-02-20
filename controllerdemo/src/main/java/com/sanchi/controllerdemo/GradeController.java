9185ee15ba6fce92c5c8e738e64c2b5cfbda89ca
sanchi285 <bismoy.sanchi@gmail.com>
2023-02-21 00:09:38 +0600

    thyme leaf loop added

--- controllerdemo/src/main/java/com/sanchi/controllerdemo/GradeController.java
+++ controllerdemo/src/main/java/com/sanchi/controllerdemo/GradeController.java
@@ -1,4 +1,7 @@
 package com.sanchi.controllerdemo;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
+
+    List<Grade> student_grade = new ArrayList<>();
+    boolean chk=false;
     @GetMapping("/grades")
     public String sayHello(Model model){
-        Grade grade = new Grade("Harry","potion","A+");
-        model.addAttribute("grade",grade);
+        if(chk==false){
+        chk=true;
+        student_grade.add(new Grade("Harry","Potion","A+"));
+        student_grade.add(new Grade("Sanchi","Dark art","A++"));
+        student_grade.add(new Grade("Ron","chess","A"));
+        }
+        model.addAttribute("grades",student_grade);
         return "grades";
     }
 }
