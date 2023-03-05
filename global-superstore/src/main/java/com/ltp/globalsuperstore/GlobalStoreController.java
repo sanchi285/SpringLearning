9b6429d622ecb3f38f6e1254cc10b6aa22ab7eb7
sanchi285 <bismoy.sanchi@gmail.com>
2023-03-05 21:42:12 +0600

    Items are added in form

--- global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalStoreController.java
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalStoreController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class GlobalStoreController {
     
     @GetMapping("/")
     public String formShow(Model model){
+       // Constants constants = new Constants();
+       model.addAttribute("items", Constants.CATEGORIES);
         return "form";
     }
 
