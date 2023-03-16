d6d224826236ac14b6055a0586ddb052538a0abc
sanchi285 <bismoy.sanchi@gmail.com>
2023-03-16 22:56:01 +0600

    Integration test added for showGradeform

--- test/grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ test/grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -1,13 +1,39 @@
 package com.ltp.gradesubmission;
 
+import static org.junit.Assert.assertNotNull;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.ltp.gradesubmission.controller.GradeController;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GradeSubmissionApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(mockMvc);
 	}
+
+	@Test
+	public void testShowGradeForm() throws Exception{
+		RequestBuilder request  = MockMvcRequestBuilders.get("/?id=123");
+		mockMvc.perform(request)
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(view().name("form"))
+			.andExpect(model().attributeExists("grade"));
+	}
+
 
 }
