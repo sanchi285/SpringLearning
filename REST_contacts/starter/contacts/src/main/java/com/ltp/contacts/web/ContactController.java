d7a6cd1fbb113704023e179fc415b4883d185635
sanchi285 <bismoy.sanchi@gmail.com>
2023-03-17 13:07:13 +0600

    First Json object returned

--- REST_contacts/starter/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ REST_contacts/starter/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
 @Controller
@@ -14,8 +16,10 @@ public class ContactController {
     private ContactService contactService;
 
     @GetMapping("/contact/{id}")
-    public void getContact(@PathVariable String id){
+    @ResponseBody
+    public Contact getContact(@PathVariable String id){
         System.out.println(id);
+        return   new Contact("123","Ram","099898");
     }
 
 }
