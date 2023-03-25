75b87bd7e8cf5380c3e6ea6aaad793a6b10c1610
sanchi285 <bismoy.sanchi@gmail.com>
2023-03-26 02:09:36 +0600

    Connected the backend with mongo db database successfully

---
+++ mongodbtest/src/main/java/com/mongodbtest/mongodbtest/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.mongodbtest.mongodbtest;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+}
