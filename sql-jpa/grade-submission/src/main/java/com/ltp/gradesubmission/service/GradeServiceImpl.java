09768bb053ef57cb6cd5e0c3714fe8e473dd558c
sanchi285 <bismoy.sanchi@gmail.com>
2023-04-01 22:01:08 +0600

    Finalize Student grade: get grades by studentId and course Id added

--- sql-jpa/grade-submission/src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
+++ sql-jpa/grade-submission/src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
@@ -61,22 +61,22 @@ public class GradeServiceImpl implements GradeService {
 
     @Override
     public void deleteGrade(Long studentId, Long courseId) {
-        
+        gradeRepository.deleteByStudentIdAndCourseId(studentId, courseId);
     }
 
     @Override
     public List<Grade> getStudentGrades(Long studentId) {
-        return null;
+        return gradeRepository.findByStudentId(studentId);
     }
 
     @Override
     public List<Grade> getCourseGrades(Long courseId) {
-        return null;
+        return gradeRepository.findByCourseId(courseId);
     }
 
     @Override
     public List<Grade> getAllGrades() {
-        return null;
+        return (List<Grade>) gradeRepository.findAll();
     }
 
 }
