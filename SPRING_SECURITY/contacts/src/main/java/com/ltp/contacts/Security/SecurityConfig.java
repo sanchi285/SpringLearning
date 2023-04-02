2bf3c8aece56914a5d692fc984e1112b7b311bb9
sanchi285 <bismoy.sanchi@gmail.com>
2023-04-02 22:08:25 +0600

    antMatcher added csrf disabled

--- SPRING_SECURITY/contacts/src/main/java/com/ltp/contacts/Security/SecurityConfig.java
+++ SPRING_SECURITY/contacts/src/main/java/com/ltp/contacts/Security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ltp.contacts.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -23,7 +24,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterchain (HttpSecurity http) throws Exception{
         http
+            .csrf().disable()
             .authorizeRequests()
+            .antMatchers(HttpMethod.DELETE).hasAnyRole("ADMIN")
             .anyRequest().authenticated()
             .and()
             .httpBasic()
