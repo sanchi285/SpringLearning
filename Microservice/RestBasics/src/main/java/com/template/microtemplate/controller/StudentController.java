f9c786d7f30cdd7293b7475e243290f6a7c87883
sanchi285 <bismoy.sanchi@gmail.com>
2023-04-08 23:39:23 +0600

    Return a list as a rest api

--- Microservice/RestBasics/src/main/java/com/template/microtemplate/controller/StudentController.java
+++ Microservice/RestBasics/src/main/java/com/template/microtemplate/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.template.microtemplate.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -11,6 +14,15 @@ public class StudentController {
     public Student getStudent(){
         Student student = new Student(1,"Ramesh","Podder");
         return student;
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List <Student> students = new ArrayList<>();
+        students.add(new Student(1,"ramesh","poddefr"));
+        students.add(new Student(2,"ramesh","pal"));
+        students.add(new Student(3,"Victor","poddefr"));
+        students.add(new Student(4,"ramesh","Das"));
+        return  students;
     }
 
 }
---
+++ Microservice/microtemplate/src/test/java/com/template/microtemplate/MicrotemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.microtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrotemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
