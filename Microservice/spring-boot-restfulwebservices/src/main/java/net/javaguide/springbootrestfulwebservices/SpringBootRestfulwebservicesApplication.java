1a4bfda61f9ea95d353ec03a1571c1a8bdf73db8
sanchi285 <bismoy.sanchi@gmail.com>
2023-04-09 16:04:40 +0600

    Spring JPA entity created and connected with mysqlWorkbench

---
+++ Microservice/spring-boot-restfulwebservices/src/main/java/net/javaguide/springbootrestfulwebservices/SpringBootRestfulwebservicesApplication.java
@@ -0,0 +1,13 @@
+package net.javaguide.springbootrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulwebservicesApplication.class, args);
+	}
+
+}
---
+++ Microservice/spring-boot-restfulwebservices/src/test/java/net/javaguide/springbootrestfulwebservices/SpringBootRestfulwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguide.springbootrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
