0851f41dae4ec3a945e684d83e6b6bde37779675
sanchi285 <bismoy.sanchi@gmail.com>
2023-04-20 02:04:02 +0600

    Load balancer hasbeen added for multiple instances

--- Microservice/BUILDING_MICROSERVICE/employee-service/src/main/java/net/java/guides/employeeservice/service/APIClient.java
+++ Microservice/BUILDING_MICROSERVICE/employee-service/src/main/java/net/java/guides/employeeservice/service/APIClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url="http://localhost:9090", value = "DEPARTMENT-SERVICE")
+@FeignClient("DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
