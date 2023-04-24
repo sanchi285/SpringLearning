20ac92dccd011d55a3eb26cf7c408484046be1a6
sanchi285 <bismoy.sanchi@gmail.com>
2023-04-24 23:55:12 +0600

    health endpoints has been exposed

--- Microservice/Resilience4jTry/ServiceA/src/main/java/com/sanchi285/ServiceA/controller/ServiceAController.java
+++ Microservice/Resilience4jTry/ServiceA/src/main/java/com/sanchi285/ServiceA/controller/ServiceAController.java
@@ -1,5 +1,6 @@
 package com.sanchi285.ServiceA.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +15,16 @@ public class ServiceAController {
     private RestTemplate restTemplate;
 
     private static final String BASE_URL = "http://localhost:8093";
+    private static final String SERVICE_A = "serviceA";
 
     @GetMapping
+    @CircuitBreaker(name = SERVICE_A, fallbackMethod = "getServiceAFallback")
     public String serviceA(){
         String url = BASE_URL + "/b";
         return restTemplate.getForObject(url,String.class);
+    }
+
+    public String getServiceAFallback(){
+        return "This is a fallback method";
     }
 }
