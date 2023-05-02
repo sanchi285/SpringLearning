0023fffdb415d8b50fd06d6984a0963d6f539228
sanchi285 <bismoy.sanchi@gmail.com>
2023-05-02 23:58:41 +0600

    Organizations Jpa entity and Spring Data jpa repo  have  been created

---
+++ Microservice/BUILDING_MICROSERVICE/organizations-service/src/main/java/com/sanchi285/organizationsservice/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package com.sanchi285.organizationsservice.repository;
+
+import com.sanchi285.organizationsservice.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
