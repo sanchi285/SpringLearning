66ec71498ff1d8da59bc4e7124ca06809056719e
sanchi285 <bismoy.sanchi@gmail.com>
2023-05-02 23:47:15 +0600

    Organization Service database has been configured

--- Microservice/BUILDING_MICROSERVICE/organizations-service/src/main/java/com/sanchi285/organizationsservice/OrganizationsServiceApplication.java
+++ Microservice/BUILDING_MICROSERVICE/organizations-service/src/main/java/com/sanchi285/organizationsservice/OrganizationsServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrganizationsServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizationsServiceApplication.class, args);
 	}
