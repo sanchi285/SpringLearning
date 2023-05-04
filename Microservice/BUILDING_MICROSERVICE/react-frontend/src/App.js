cc25301258119ebe86b291afac07154b2b3d06b6
sanchi285 <bismoy.sanchi@gmail.com>
2023-05-05 00:40:40 +0600

    Basic APi Call Completed

--- Microservice/BUILDING_MICROSERVICE/react-frontend/src/App.js
+++ Microservice/BUILDING_MICROSERVICE/react-frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import EmployeeComponent from './component/EmployeeComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hello Spring</h1>
+       <EmployeeComponent/>
       </header>
     </div>
   );
