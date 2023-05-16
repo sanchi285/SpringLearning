4fadbbd0fd5ea6bc2d83f4843f30afeb77ddaa49
sanchi285 <bismoy.sanchi@gmail.com>
2023-05-16 23:57:02 +0600

    Kafka started

---
+++ Microservice/springboot-kafka-tutorial/src/main/java/net/javaguides/springbootkafkatutorial/SpringbootKafkaTutorialApplication.java
@@ -0,0 +1,12 @@
+package net.javaguides.springbootkafkatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTutorialApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
+	}
+
+}
