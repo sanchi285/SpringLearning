e12849b89b965403c1f2a2719d8f007d81d160d4
sanchi285 <bismoy.sanchi@gmail.com>
2023-07-19 00:55:13 +0600

    Deleted old

--- Microservice/BUILDING_MICROSERVICE/employee-service/src/main/java/net/java/guides/employeeservice/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package net.java.guides.employeeservice.repository;
-
-import net.java.guides.employeeservice.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
--- global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.ltp.globalsuperstore;
-
-public class Constants {
-    public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
-    public static final String SUCCESS = "success";
-}
