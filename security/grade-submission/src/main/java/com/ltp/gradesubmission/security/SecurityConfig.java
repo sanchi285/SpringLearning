29810277f8c9739a18aba0e3569861a25766028b
sanchi285 <bismoy.sanchi@gmail.com>
2023-08-02 01:06:23 +0600

    Authentication Manager Code Has been Completed

--- security/grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
+++ security/grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.ltp.gradesubmission.security;
 
 
+import com.ltp.gradesubmission.security.filter.AuthenticationFilter;
+import com.ltp.gradesubmission.security.filter.ExceptionHandlerFilter;
+import com.ltp.gradesubmission.security.manager.CustomAuthenticationManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,17 +18,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @AllArgsConstructor
 public class SecurityConfig {
 
+
+    private CustomAuthenticationManager customAuthenticationManager;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationFilter authenticationFilter =  new AuthenticationFilter(customAuthenticationManager);
+        authenticationFilter.setFilterProcessesUrl("/authenticate");
+
         http        
             .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
             .and()
             .csrf().disable()
             .authorizeRequests()
             .antMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+
                 .antMatchers(HttpMethod.POST,SecurityConstants.REGISTER_PATH).permitAll()
                 .anyRequest().authenticated()
             .and()
+                .addFilterBefore(new ExceptionHandlerFilter(),AuthenticationFilter.class)
+                .addFilter(authenticationFilter)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
--- security/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
+++ security/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
@@ -1,5 +1,50 @@
 package com.ltp.gradesubmission.security.filter;
 
-public class AuthenticationFilter {
-    
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ltp.gradesubmission.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@AllArgsConstructor
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response){
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword());
+            authenticationManager.authenticate(authentication);
+            System.out.println(user.getUsername());
+            System.out.println(user.getPassword());
+            return authenticationManager.authenticate(authentication);
+        }
+
+        catch (Exception e){
+            throw  new RuntimeException();
+        }
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+    }
+
 }
