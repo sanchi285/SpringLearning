010e41f9dae842b25f5ef824631a5f880db28508
sanchi285 <bismoy.sanchi@gmail.com>
2023-08-07 00:26:14 +0600

    Buid SuccessFull

--- MicroProject/productservice/src/main/java/com/programmingtechie/productservice/Controller/ProductController.java
+++ MicroProject/productservice/src/main/java/com/programmingtechie/productservice/Controller/ProductController.java
@@ -1,10 +1,14 @@
 package com.programmingtechie.productservice.Controller;
 
 import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.dto.ProductResponse;
 import com.programmingtechie.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/product")
@@ -17,6 +21,12 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
         productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProduct(){
+       return productService.getAllProducts();
     }
 
 }
--- MicroProject/productservice/src/main/java/com/programmingtechie/productservice/service/ProductService.java
+++ MicroProject/productservice/src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -1,12 +1,16 @@
 package com.programmingtechie.productservice.service;
 
 import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.dto.ProductResponse;
 import com.programmingtechie.productservice.model.Product;
 import com.programmingtechie.productservice.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,4 +29,17 @@ public class ProductService {
         log.info("Product {} is saved",product.getId());
     }
 
+    public List<ProductResponse> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        return  products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product){
+            return ProductResponse.builder()
+                    .id(product.getId())
+                    .name(product.getName())
+                    .description(product.getDescription())
+                    .price(product.getPrice())
+                    .build();
+    }
 }
